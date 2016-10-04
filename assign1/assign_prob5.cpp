9c3fdd5d74f508dfbaf020621831843febddf769
Robert Dudasik <robertadudasik@lewisu.edu>
2016-10-04 16:00:28 -0500

    Update assign_prob5.cpp

--- assign1/assign_prob5.cpp
+++ assign1/assign_prob5.cpp
@@ -2,11 +2,11 @@
 Title:				assign_prob5.cpp
 Author:				Ethan Blatti, Nick Biegel, Robert Dudasik
 Created on:			October 4, 2016
-Description:		Generates a report for a user's savings account with the data provided.
-Purpose:			Generate a report for a savings account using data provided by the user.
-Usage:				Generate a report by taking user input and manipulating it.
-					Build with:	Visual Studio 2015
-Modifications:		None
+Description:			Call a function through a pointer to verify that it works.
+Purpose:			To verify that ascVal() works when called through a pointer.
+Usage:				Run the program to verify that ascVal() determines the ASCII value of a character.
+Build with:			Visual Studio 2015
+Modifications:			None
 ******************************************************************************/
 #include <iostream>
 #include <cstring>
