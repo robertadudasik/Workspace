b3936b901b4fc957b417388218d54558470970f5
Nicholas Biegel <nicholasabiegel@lewisu.edu>
2016-10-04 19:37:52 -0500

    Version 1

--- assign1/assign_prob5.cpp
+++ assign1/assign_prob5.cpp
@@ -1,44 +1,40 @@
-
 /******************************************************************************
 Title:				assign_prob5.cpp
 Author:				Ethan Blatti, Nick Biegel, Robert Dudasik
 Created on:			October 4, 2016
-Description:			Call a function through a pointer to verify that it works.
+Description:		Call a function through a pointer to verify that it works.
 Purpose:			To verify that ascVal() works when called through a pointer.
 Usage:				Run the program to verify that ascVal() determines the ASCII value of a character.
 Build with:			Visual Studio 2015
-Modifications:			None
+Modifications:		None
 ******************************************************************************/
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
+//Return the ASCII value of the char
 int ascVal(size_t ip, const char* p)
 {
-	// Return the ASCII value of the char
-	if(!p || ip > strlen(p)) {
+	cout << strlen(p) << endl;
+	if (!p || ip >= strlen(p)) {
 		return  -1;
-	}
-	else {
-	return p[ip];
+	} else {
+		return p[ip];
 	}
 }
 
 int main() {
-
 	//create pointer to function
-	int(*value)(size_t,const char*);
+	int(*value)(size_t, const char*);
 	value = &ascVal;
 
-
-	//create character and pointer to character to test
-	char character = 'a';
-	char* pchar = &character;
+	//create array of characters to test
+	char pchar[]{"This is a test string."};
 
 	//call the pointer to the function and generate output
-	cout << "The ascii value of " << character << " is: " << value(0, pchar) << endl;
-	
+	cout << "The ascii value of " << pchar[21] << " is: " << value(21, pchar) << endl;
+
 	system("PAUSE");
 	return 0;
 }
