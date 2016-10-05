619e4d64d108dfd3067d4e7892ba211b48cf86a8
Nicholas Biegel <nicholasabiegel@lewisu.edu>
2016-10-04 20:18:10 -0500

    Version 1

--- assign1/assign_prob2.cpp
+++ assign1/assign_prob2.cpp
@@ -7,35 +7,45 @@ Description:		Enter a unlimited number of characters into the program. Once the 
 					the user entered.
 Purpose:			Demonstrates using a do while loop to gather input from the user 
 					and a switch statment to count the vowels that were entered.
-Usage:				Enter integers into the program, to end data entering, enter anything other than an integer.
-					Build with:	Visual Studio 2015
+Usage:				Enter chars into the program, to end data entering, q or Q
+Build with:			Visual Studio 2015
 Modifications:		None
 ******************************************************************************/
 #include <iostream>
+
 using namespace std;
 
+//Main
 int main() {
+	//create char variable and vowel counter
 	char c;
 	int vCnt = 0;
+	
+	//get the next char from the user as long as it is not
+	//a Q or q
 	do {
+		//since Q is not a vowel, it does not matter if it goes through the loop
 		cout << "Enter a character: ";
 		cin >> c;
+		
+		//if the char is any of the cases, increment the vowel counter by 1
 		switch (c) {
-		case 'A':
-		case 'E':
-		case 'I':
-		case 'O':
-		case 'U':
-		case 'a':
-		case 'e':
-		case 'i':
-		case 'o':
-		case 'u':
-			vCnt++;
-			break;
+			case 'A':
+			case 'E':
+			case 'I':
+			case 'O':
+			case 'U':
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+				vCnt++;
+				break;
 		}
 	} while (c != 'Q' && c != 'q');
 
+	//print out vowel count
 	cout << "There were " << vCnt << " vowels in the characters you entered." << endl;
 	
 	system("PAUSE");
