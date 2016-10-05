1faa2716d4e261c2a4a0a6fae1974171e5d76f9e
Nicholas Biegel <nicholasabiegel@lewisu.edu>
2016-10-04 20:01:19 -0500

    Version 1

--- assign1/assign_prob4.cpp
+++ assign1/assign_prob4.cpp
@@ -2,7 +2,7 @@
 Title:				assign_prob4.cpp
 Author:				Ethan Blatti, Nick Biegel, Robert Dudasik
 Created on:			September 24, 2016
-Description:		Prints out each section of the string provided by the delimiter 
+Description:		Prints out each section of the string provided by the delimiter
 					provided.
 Purpose:			Demonstrates the C like function of strtok
 Usage:				Enter integers into the program, to end data entering, enter anything other than an integer.
@@ -10,31 +10,49 @@ Usage:				Enter integers into the program, to end data entering, enter anything 
 Modifications:		None
 ******************************************************************************/
 #include <iostream>
+
 using namespace std;
 
+//Function Prototypes
+/*
+	m_strtok simulates the c like strtok function.
+	Returns first part of the provided string up to the
+	delimiter. Returns NULL if nothing is to be found.
+*/
 char* m_strtok(char* str, char* delim);
 
+//Main
 int main() {
-	char data[]{"one two three"};
+	//create character array and delimiter value
+	char data[]{ "one two three" };
 	char delim = ' ';
+
+	//get the first string token value
 	char* tok = m_strtok(data, &delim);
+
+	//while the token is not null, print the token and get the next token
 	while (tok != NULL) {
 		cout << tok << endl;
 		tok = m_strtok(NULL, &delim);
 	}
-	
+
 	system("PAUSE");
 	return 0;
 }
 
+//Function definitions
 char* m_strtok(char* str, char* delim) {
+	//create static pointer to the provided character array and local temp char pointer
 	static char* string;
 	char* tmp;
+
+	//set str to it's appropriate value, else return NULL since we are at the end.
 	if (str == NULL && string != NULL)
 		str = string;
-	else if(str == NULL)
+	else if (str == NULL)
 		return NULL;
 
+	//set temp to NULL, then look for first instance of the delimiter
 	tmp = NULL;
 	for (int i = 0; i < sizeof(str) / sizeof(char); i++) {
 		if (str[i] == *delim) {
@@ -43,11 +61,16 @@ char* m_strtok(char* str, char* delim) {
 		}
 	}
 
+	//if delimiter was found, set to NULL and then set the static var to
+	//char location after the delimiter. Otherwise the string is NULL
 	if (tmp) {
 		*tmp = '\0';
 		string = tmp + 1;
-	} else {
+	}
+	else {
 		string = NULL;
 	}
+
+	//return the token
 	return str;
 }
